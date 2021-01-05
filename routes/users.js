73e4638c43dde9327bd06d6af0544ecade2937ae
lpratheesh <lalpratheesh.27@gmail.com>
2021-01-05 19:38:41 +0530

    csv export and datatables modifications.

--- routes/users.js
+++ routes/users.js
@@ -5,14 +5,15 @@ const db = require('../db');
 router.post('/csvupload_csvfiledata', async (req, res, next) => {
   try {
     const {start = 0, length = 10} = req.body;
-    const offset = +start * length;
+    const offset = +start;
     const pageLimit = length;
     const query1 = 'select * from public.csvupload_csvfiledata order by id asc offset $1 limit $2';
     const result = await db.query(query1, [+offset, +pageLimit]);
-    // const recordsTotal = await db.query('select count(*) from public.csvupload_csvfiledata');
+    const recordsTotal = await db.query('select count(*) from public.csvupload_csvfiledata');
     return res.json({
       message: 'done',
-      recordsTotal: 1100000,
+      recordsTotal: recordsTotal.rows[0].count,
+      recordsFiltered: recordsTotal.rows[0].count,
       data: result.rows
     });
   } catch (err) {
@@ -27,14 +28,15 @@ router.post('/csvupload_csvfiledata', async (req, res, next) => {
 router.post('/csvupload_uploadnew', async (req, res, next) => {
   try {
     const {start = 0, length = 10} = req.body;
-    const offset = +start * length;
+    const offset = +start;
     const pageLimit = length;
     const query2 = 'select * from public.csvupload_uploadnew order by id asc offset $1 limit $2';
     const result = await db.query(query2, [+offset, +pageLimit]);
-    // const recordsTotal = await db.query('select count(*) from public.csvupload_uploadnew');
+    const recordsTotal = await db.query('select count(*) from public.csvupload_uploadnew');
     return res.json({
       message: 'done',
-      recordsTotal: 1100000,
+      recordsTotal: recordsTotal.rows[0].count,
+      recordsFiltered: recordsTotal.rows[0].count,
       data: result.rows
     });
   } catch (err) {
